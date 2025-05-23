4350057d09fdeb2737397f3f1df227fe0c1c2518
moonbreezelate <moonbreezelate@gmail.com>
2025-05-23 12:39:37 +0800

    Update shared_ptr.hh

--- kernel/include/core/shared_ptr.hh
+++ kernel/include/core/shared_ptr.hh
@@ -65,23 +65,23 @@ class enable_shared_from_this {
   template <typename U>
   friend class shared_ptr;
 
+  friend enable_shared_from_this* enable_shared_from_this_base(control_block* /* unused */,
+                                                               enable_shared_from_this* ptr) {
+    return ptr;
+  }
+
 public:
   shared_ptr<T> shared_from_this() { return weak_this_.lock(); }
 
   shared_ptr<const T> shared_from_this() const { return weak_this_.lock(); }
 
 private:
-  weak_ptr<T> weak_this_;
-
-  friend enable_shared_from_this* enable_shared_from_this_base(control_block* /* unused */,
-                                                               enable_shared_from_this* ptr) {
-    return ptr;
-  }
-
   template <typename U>
   void weak_assign(U* ptr, control_block* cb) noexcept {
     weak_this_.assign(ptr, cb);
   }
+
+  weak_ptr<T> weak_this_;
 };
 
 // shared_ptr internal class.
@@ -99,7 +99,7 @@ class control_block_impl final : public control_block {
 
   T* object() const noexcept { return &obj_; }
 
-  void do_release() override final { obj_.~T(); }
+  void do_release() final { obj_.~T(); }
 
   T obj_;
 };
@@ -113,7 +113,7 @@ class raw_ptr_guarder final : public control_block {
 
   raw_ptr_guarder(T* ptr) noexcept : ptr_(ptr) {}
 
-  void do_release() override final { delete ptr_; }
+  void do_release() final { delete ptr_; }
 
   T* ptr_;
 };
@@ -123,29 +123,13 @@ class shared_ptr {
   template <typename U>
   friend class weak_ptr;
 
-  template <typename Yp>
-  using esft_base_t = decltype(enable_shared_from_this_base(std::declval<control_block*>(), std::declval<Yp*>()));
-
-  template <typename Yp, typename = void>
-  struct has_esft_base : std::false_type {};
-
-  template <typename Yp>
-  struct has_esft_base<Yp, std::__void_t<esft_base_t<Yp>>> : std::true_type {};
-
-  template <typename Yp, typename Yp2 = std::remove_cv_t<Yp>>
-  typename std::enable_if_t<has_esft_base<Yp2>::value> enable_shared_from_this_with(Yp* ptr) noexcept {
-    if (auto base = enable_shared_from_this_base(cb_, ptr)) {
-      base->weak_assign(static_cast<Yp2*>(ptr), cb_);
-    }
-  }
-
-  template <typename Yp, typename Yp2 = std::remove_cv_t<Yp>>
-  typename std::enable_if_t<!has_esft_base<Yp2>::value> enable_shared_from_this_with(Yp* /*unused*/) noexcept {}
-
 public:
+  using element_type = T;
+
   shared_ptr() noexcept = default;
 
-  explicit shared_ptr(T* ptr) noexcept {
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U*, T*>>>
+  explicit shared_ptr(U* ptr) noexcept {
     if (ptr) {
       cb_  = new raw_ptr_guarder(ptr);
       ptr_ = ptr;
@@ -168,29 +152,18 @@ public:
     enable_shared_from_this_with(ptr_);
   }
 
-  shared_ptr(const shared_ptr& other) : cb_(other.cb_), ptr_(other.ptr_) {
-    if (cb_) {
-      cb_->use_add();
-    }
-  }
-
-  shared_ptr(shared_ptr&& other) noexcept
-      : cb_(std::exchange(other.cb_, nullptr)),
-        ptr_(std::exchange(other.ptr_, nullptr)) {}
-
-  shared_ptr(const weak_ptr<T>& weak) {
-    if (auto other = weak.lock()) {
-      new (this) shared_ptr(std::move(other));
-    }
-  }
-
   template <typename U, typename = std::enable_if_t<std::is_convertible_v<U*, T*>>>
   shared_ptr(const shared_ptr<U>& other) : cb_(other.cb_),
                                            ptr_(other.ptr_) {
-    if (cb_) {
-      cb_->use_add();
-    }
+    do_add_use();
   }
+
+  template <typename U, typename = std::enable_if_t<std::is_convertible_v<U*, T*>>>
+  shared_ptr(shared_ptr<U>&& other) noexcept
+      : cb_(std::exchange(other.cb_, nullptr)),
+        ptr_(std::exchange(other.ptr_, nullptr)) {}
+
+  shared_ptr(const weak_ptr<T>& other) : cb_(other.cb_), ptr_(other.ptr_) { do_add_use(); }
 
   ~shared_ptr() { reset(); }
 
@@ -229,6 +202,34 @@ public:
   explicit operator bool() const noexcept { return cb_ != nullptr; }
 
 private:
+  /* begin traits enable_shared_from_this::enable_shared_from_this_base  */
+  template <typename Yp>
+  using esft_base_t = decltype(enable_shared_from_this_base(std::declval<control_block*>(), std::declval<Yp*>()));
+
+  template <typename Yp, typename = void>
+  struct has_esft_base : std::false_type {};
+
+  template <typename Yp>
+  struct has_esft_base<Yp, std::__void_t<esft_base_t<Yp>>> : std::true_type {};
+
+  template <typename Yp, typename Yp2 = std::remove_cv_t<Yp>>
+  typename std::enable_if_t<has_esft_base<Yp2>::value> enable_shared_from_this_with(Yp* ptr) noexcept {
+    if (auto base = enable_shared_from_this_base(cb_, ptr)) {
+      base->weak_assign(static_cast<Yp2*>(ptr), cb_);
+    }
+  }
+
+  template <typename Yp, typename Yp2 = std::remove_cv_t<Yp>>
+  typename std::enable_if_t<!has_esft_base<Yp2>::value> enable_shared_from_this_with(Yp* /*unused*/) noexcept {}
+
+  /* end traits enable_shared_from_this::enable_shared_from_this_base  */
+
+  void do_add_use() {
+    if (cb_) {
+      cb_->use_add();
+    }
+  }
+
   control_block* cb_ {};
   T*             ptr_ {};
 };
@@ -295,9 +296,9 @@ public:
 
   shared_ptr<T> lock() const noexcept {
     if (cb_ && cb_->use_count() > 0) {
-      return shared_ptr<T>(*this);
+      return {*this};
     }
-    return shared_ptr<T>();
+    return {};
   }
 
   std::size_t use_count() const noexcept { return cb_ ? cb_->use_count() : 0; }
@@ -344,6 +345,24 @@ bool operator!=(const bull::shared_ptr<T>& lhs, std::nullptr_t /*unused*/) noexc
 template <typename T>
 bool operator!=(std::nullptr_t /*unused*/, const bull::shared_ptr<T>& rhs) noexcept {
   return static_cast<bool>(rhs.get());
+}
+
+template <typename T, typename U>
+inline shared_ptr<T> static_pointer_cast(const shared_ptr<U>& from) noexcept {
+  return {from, static_cast<typename shared_ptr<T>::element_type*>(from.get())};
+}
+
+template <typename T, typename U>
+inline shared_ptr<T> const_pointer_cast(const shared_ptr<U>& from) noexcept {
+  return {from, const_cast<typename shared_ptr<T>::element_type*>(from.get())};
+}
+
+template <typename T, typename U>
+inline shared_ptr<T> dynamic_pointer_cast(const shared_ptr<U>& from) noexcept {
+  if (auto* to = dynamic_cast<typename shared_ptr<T>::element_type*>(from.get())) {
+    return {from, to};
+  }
+  return {};
 }
 
 }  // namespace bull
